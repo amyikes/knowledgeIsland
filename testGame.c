026bdd2cefca4e01a752855d12aca6fb769622d1
amymdiaz <48150058+amymdiaz@users.noreply.github.com>
2019-06-05 17:18:57 +1000

    Update testGame.c

--- testGame.c
+++ testGame.c
@@ -11,17 +11,45 @@
                 STUDENT_MMONEY, STUDENT_MTV, STUDENT_BQN, STUDENT_BPS}
 #define DEFAULT_DICE {9,10,8,12,6,5,3,11,3,11,4,6,4,7,9,2,8,10,5}
 
+
+int check(int didPass, const char *testDescription) {
+    fprintf(stderr, testDescription);
+    fprintf(stderr, "... ");
+    if (!didPass) {
+        fprintf(stderr, "failed!\n");
+    } else {
+        fprintf(stderr, "passed!\n");
+    }
+    return !didPass;
+}
+
+
+
 int main(int argc, char *argv[]) {
-  int disciplines[] = DEFAULT_DISCIPLINES;
-  int dice[] = DEFAULT_DICE;
-  Game g = newGame (disciplines, dice);
 
-	// TODO: write your tests on the Game here, using asserts
+    int disciplines[] = DEFAULT_DISCIPLINES;
+    int dice[] = DEFAULT_DICE;
 
-	// clean up: keep your memory beautiful
-	disposeGame(g);
+    int numFailed = 0;
+    
+    Game g = newGame(disciplines, dice);
 
-	// TODO: all tests passed message :)
 
+    // TODO: write your tests on the Game here, using asserts
+
+    //checking stuff for first turn...
+    numFailed += check(getTurnNumber(g) == -1, "Checking start turn number is -1");
+    numFailed += check(getWhoseTurn(g) == NO_ONE, "Checking it is no ones turn");
+    numFailed += check(getMostPublications(g) == NO_ONE, "Checking that no one has most publications");
+    numFailed += check(getMostARCs(g) == NO_ONE, "Checking no one has most ARCs");
+
+    // clean up: keep your memory beautiful
+    disposeGame(g);
+
+    
+    assert(numFailed == 0);
+    printf("All tests passed, you are Awesome!\n");
 	return EXIT_SUCCESS;
+
+    return EXIT_SUCCESS;
 }
